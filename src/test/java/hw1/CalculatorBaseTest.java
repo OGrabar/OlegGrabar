aa590e8b74e9d03384706b6fb2a200077d189d09
Oleg.Grabar <oleg.grabar@nordidgy.ru>
2021-03-21 16:09:07 +0300

    HW 1. Attempt 2. Fix annotations in base test class

--- src/test/java/hw1/CalculatorBaseTest.java
+++ src/test/java/hw1/CalculatorBaseTest.java
@@ -1,20 +1,19 @@
 package hw1;
 
 import com.epam.tat.module4.Calculator;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 public class CalculatorBaseTest {
 
     protected Calculator calculator;
     protected static final double DOUBLE_DELTA = 1e-6;
 
-    @BeforeSuite
+    @BeforeClass
     public void setUp() {
         calculator = new Calculator();
     }
 
-    @AfterSuite
+    @AfterClass
     public void tearDown() {
         calculator = null;
     }
